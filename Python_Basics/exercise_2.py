33b89a07eb9498b4c0e4f38a311f11ff829e0e4b
Misbah Iqbal <misbah.iqbal@itb.local>
2022-06-02 12:57:12 +0500

    variable practise

---
+++ Python_Basics/exercise_2.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  2 12:51:56 2022
+
+@author: misbah.iqbal
+"""
+
+
+#Thsi is a variable practsise exercise 
+
+
+hello = "Its my second exercise"
+print(hello)
