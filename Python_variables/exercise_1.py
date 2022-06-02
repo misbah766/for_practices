355680fea0483216681c3f7cbf7a797dc868a446
Misbah Iqbal <misbahiqbal76@gmail.com>
2022-06-02 15:43:16 +0500

    variable assignment

---
+++ Python_variables/exercise_1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  2 15:26:28 2022
+
+@author: misbah.iqbal
+"""
+
+#Exercise 1 
+
+counter = 100 # integer
+miles = 1000.0 # floating point
+name = "jack"  #string
+
+print(counter,miles,name)
